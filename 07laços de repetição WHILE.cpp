d5fc9dd62ef42866950196dcc862af40b361292d
Vitor Santos <100627603+fliperamav8@users.noreply.github.com>
2022-11-20 18:13:17 -0300

    Add files via upload

---
+++ 07laços de repetição WHILE.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/* while(expressão logica boleana){
+ "SEU CÓDIGO QUE SERA REPETIDO"
+}
+ */
+
+// PARA PARAR PROCESSO DE LOOPING INFINITO USAR "(variavel)++;"exemplo (i++;)
+// (i++;) é mesma coisa que: i = i + 1; 
+
+int main(){
+
+    int x = 0;
+
+while (x <=0 6){
+   
+    cout <<  x << endl;
+    
+    x++;
+}
+cout << "fim da execucao" << endl;
+
+	return 0;
+}
